0cf25d5dc0d155ec50a2a4d3542d464fda36c44b
ANUMIT JOOLOOR <anumit@gmail.com>
2025-01-11 03:47:38 +0530

    Add files via upload

--- Java-Basics/Array/Array1.java
+++ Java-Basics/Array/Array1.java
@@ -5,9 +5,18 @@ Online Java - IDE, Code Editor, Compiler
 Online Java is a quick and easy tool that helps you to build, compile, test your programs online.
 */
 
-public class Main
-{
+public class Array1 {
+    
     public static void main(String[] args) {
         System.out.println("Welcome to Online IDE!! Happy Coding :)");
+        
+        // Declare and initialize an integer array with 5 numbers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print the array elements
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
     }
+    
 }
