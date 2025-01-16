f557e65455df876b657aad63fd220415b75184c0
ANUMIT JOOLOOR <anumit@gmail.com>
2025-01-16 13:39:28 +0530

    Add files via upload

---
+++ Java-Basics/HelloWorld2.java
@@ -0,0 +1,25 @@
+/*
+Welcome to JDoodle!
+
+You can execute code here in 88 languages. Right now you’re in the Java IDE.
+
+  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
+
+  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
+
+  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
+
+Want to change languages? Try the search bar up the top.
+*/
+
+public class HelloWorld2 {
+  public static void main(String args[]) {
+    
+    
+    // This is a comment
+    System.out.println("Hello World");
+
+    
+    
+  }
+}
