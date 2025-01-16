818ccf24118d7aeb6f09b89199d513c0e786cc68
ANUMIT JOOLOOR <anumit@gmail.com>
2025-01-16 16:29:32 +0530

    Add files via upload

--- Java-Basics/HelloWorld2.java
+++ Java-Basics/HelloWorld2.java
@@ -1,16 +1,5 @@
-/*
-Welcome to JDoodle!
+import java.util.*;
 
-You can execute code here in 88 languages. Right now you’re in the Java IDE.
-
-  1. Click the orange Execute button ▶ to execute the sample code below and see how it works.
-
-  2. Want help writing or debugging code? Type a query into JDroid on the right hand side ---------------->
-
-  3.Try the menu buttons on the left. Save your file, share code with friends and open saved projects.
-
-Want to change languages? Try the search bar up the top.
-*/
 
 public class HelloWorld2 {
     
@@ -20,7 +9,25 @@ public class HelloWorld2 {
     // This is a comment
     System.out.println("Hello World 2");
 
+    String name = "John";
+    System.out.println(name);
     
+    int myNum = 15;
+    System.out.println(myNum);
     
+    int myNum2 = 15;
+    myNum2 = 20;  // myNum is now 20
+    System.out.println(myNum2);
+    
+    final int myNum3 = 15;
+    // myNum3 = 20;  // will generate an error: cannot assign a value to a final variable
+
+int myNum4 = 5;
+float myFloatNum = 5.99f;
+char myLetter = 'D';
+boolean myBool = true;
+String myText = "Hello";
+      
+      
   }
 }
