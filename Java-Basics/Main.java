94df0ba9e8f25bc685f4ace53d2255361b0adc82
ANUMIT JOOLOOR <anumit@gmail.com>
2025-01-16 12:48:07 +0530

    Add files via upload

---
+++ Java-Basics/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World");
+        System.out.println("Hello World!");
+        System.out.println("I am learning Java.");
+        System.out.println("It is awesome!");
+        
+    }
+}
