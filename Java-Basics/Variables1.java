4f556f880cd167a23fee75c6b1dede8b16b59d7c
ANUMIT JOOLOOR <anumit@gmail.com>
2025-01-16 16:46:46 +0530

    Add files via upload

---
+++ Java-Basics/Variables1.java
@@ -0,0 +1,24 @@
+
+
+
+public class Variables1 {
+  public static void main(String args[]) {
+
+String name = "John";
+System.out.println("Hello " + name);
+
+String firstName = "John ";
+String lastName = "Doe";
+String fullName = firstName + lastName;
+System.out.println(fullName);
+
+int x = 5;
+int y = 6;
+System.out.println(x + y); // Print the value of x + y
+
+// Good
+int minutesPerHour = 60;
+
+
+  }
+}
