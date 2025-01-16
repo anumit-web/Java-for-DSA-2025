0c8154ee57f2a926f95ad9e8d38f5b3333119816
ANUMIT JOOLOOR <anumit@gmail.com>
2025-01-16 17:56:11 +0530

    Add files via upload

---
+++ Java-Basics/Variables2.java
@@ -0,0 +1,42 @@
+
+
+
+public class Variables2 {
+  public static void main(String args[]) {
+
+   String name = "John";
+   System.out.println("Hello " + name);
+   
+// Create integer variables
+int length = 4;
+int width = 6;
+int area;
+
+// Calculate the area of a rectangle
+area = length * width;
+
+System.out.println("Area of the rectangle is: " + area);
+
+long myNum = 15000000000L;
+System.out.println(myNum);
+ 
+double myNum2 = 19.99d;
+System.out.println(myNum2); 
+ 
+float f1 = 35e3f;
+double d1 = 12E4d;
+System.out.println(f1);
+System.out.println(d1);
+
+boolean isJavaFun = true;
+boolean isFishTasty = false;
+System.out.println(isJavaFun);     // Outputs true
+System.out.println(isFishTasty);   // Outputs false
+
+char myGrade = 'B';
+System.out.println(myGrade);
+ 
+ 
+ 
+  }
+}
