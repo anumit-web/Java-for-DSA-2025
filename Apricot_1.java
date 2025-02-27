52a66a033154cebc5c6c68c77703d26a1c77cdca
ANUMIT JOOLOOR <anumit@gmail.com>
2025-02-27 11:42:39 +0530

    Add files via upload

---
+++ Apricot_1.java
@@ -0,0 +1,40 @@
+/******************************************************************************
+
+                            Online Java Debugger.
+                Code, Run and Debug Java program online.
+Write your code in this editor and press "Debug" button to debug program.
+
+*******************************************************************************/
+
+// https://www.onlinegdb.com/online_java_debugger#
+
+import java.util.Arrays;
+
+public class Main
+{
+	public static void main(String[] args) {
+	    
+		System.out.println("Hello World");
+		
+		System.out.println("Welcome to Online IDE!! Happy Coding :)");
+		
+        String[] fruits1 = {"grapes", "apricot", "strawberry", "cranberry", "kiwi"};
+        
+        String[] fruits2 = new String[3];
+        
+        // print an array elemnt
+        System.out.println(fruits1[2]);
+        
+        // change an array element
+        fruits1[2] = "pineapple";
+        System.out.println(fruits1[2]);
+        
+        // Convert String Array to String
+        System.out.println(Arrays.toString(fruits1));
+        
+        // convert string arrays to list
+        List<String> fruitsList = Arrays.asList(fruits1); 
+        
+        
+	}
+}
