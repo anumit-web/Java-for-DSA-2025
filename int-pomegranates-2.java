2ecd320ac6033a7ad4d749b40b74d0f2eb6daa56
ANUMIT JOOLOOR <anumit@gmail.com>
2025-03-02 15:58:30 +0530

    Add files via upload

--- int-pomegranates-2.java
+++ int-pomegranates-2.java
@@ -6,21 +6,59 @@ C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS
 Code, Compile, Run and Debug online from anywhere in world.
 
 *******************************************************************************/
+
+// https://www.onlinegdb.com/edit/aGSUpziOh
+// int-pomegranates-2.java
+  
+  
+import java.util.Arrays;
+
 public class Main
 {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		
+
 		int[] candySales = new int[3];
 		candySales[0] = 30;
 		candySales[1] = 60;
 		candySales[2] = 80;
-		
-        for (int i = 0; i < candySales.length; i++) {
-            System.out.println("*=" + candySales[i]);
+
+		for (int i = 0; i < candySales.length; i++) {
+			System.out.println("*=" + candySales[i]);
+		}
+
+        // 2 D arrays in Java
+
+        // declare the variable and allocate memory
+        int[][] myPita = new int[4][2];
+        myPita[2][1] = 99;
+        myPita[3][0] = 55;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                System.out.print(myPita[i][j] + " ");
+            }
+            System.out.println();
+        }
+ 
+        System.out.println("____");
+
+        // 3 D int array
+        // declare the variable and allocate memory
+        int[][][] myGhriba = new int[4][2][3];
+        myGhriba[2][1][2] = 1;
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 2; j++) {
+                for (int k = 0; k < 3; k++) {
+                    System.out.print(myGhriba[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println("____");
         }
 
-		
-		
+        // print %''
+        int[] intArray = { 7, 9, 5, 1, 3 };
+        System.out.println( "$="+Arrays.toString(intArray));
+
 	}
 }
