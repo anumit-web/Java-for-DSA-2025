ec22d6101aea2f307c5d9212954acc49c3767026
ANUMIT JOOLOOR <anumit@gmail.com>
2025-03-02 15:15:46 +0530

    Add files via upload

---
+++ int-pomegranates-2.java
@@ -0,0 +1,26 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		
+		int[] candySales = new int[3];
+		candySales[0] = 30;
+		candySales[1] = 60;
+		candySales[2] = 80;
+		
+        for (int i = 0; i < candySales.length; i++) {
+            System.out.println("*=" + candySales[i]);
+        }
+
+		
+		
+	}
+}
