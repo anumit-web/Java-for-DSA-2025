4b0615fa96794a7510074bc8a976914a7a676fc8
ANUMIT JOOLOOR <anumit@gmail.com>
2025-03-10 18:45:02 +0530

    Add files via upload

---
+++ Java-HashMap-string-and-string.java
@@ -0,0 +1,51 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+//Country food
+import java.util.HashMap;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		HashMap<String, String> countryFood = new HashMap<String, String>();
+
+		// Key value pairs
+		// Add keys and values (Country, City)
+		countryFood.put("Italy", "Pizza");
+		countryFood.put("Japan", "Ramen");
+		countryFood.put("Malaysia", "Nasi Lemak");
+		countryFood.put("USA", "Hamburger");
+		countryFood.put("Canada", "Tim Hortons");
+		System.out.println(countryFood);
+
+		// Access an Item
+		System.out.println("*="+countryFood.get("Japan"));
+
+
+		// Remove an Item
+		System.out.println("-/-"+ countryFood.remove("China"));
+		System.out.println("-/-"+ countryFood.remove("Japan"));
+
+		// HashMap Size
+		System.out.println("o:o ="+ countryFood.size());
+
+		// Loop Through a HashMap
+		for (String item : countryFood.keySet()) {
+			System.out.println(".-----. = " + item);
+		}
+
+
+		// Print keys and values
+		for (String item : countryFood.keySet()) {
+			System.out.println("key = " + item + "; it's value = " + countryFood.get(item));
+		}
+
+	}
+}
