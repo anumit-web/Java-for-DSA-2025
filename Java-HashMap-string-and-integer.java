cb96b8d4f6a49ea2d7ebbf4f24785f3a0b9f954d
ANUMIT JOOLOOR <anumit@gmail.com>
2025-03-12 11:46:15 +0530

    Add files via upload

---
+++ Java-HashMap-string-and-integer.java
@@ -0,0 +1,98 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+
+// https://www.onlinegdb.com/edit/wyNZ9WhTp
+/*
+Java HashMap
+String, Integer
+
+Java docs link =
+https://docs.oracle.com/javase/8/docs/api/java/util/HashMap.html
+
+Chinese mail order sorting facility photo -
+https://itsupplychain.com/mini-yellow-robots-deliver-the-right-package/
+
+*/
+
+
+import java.util.HashMap;
+
+public class Main
+{
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+
+		/*
+		In Java, a HashMap can store key-value pairs,
+		where the keys are strings and the values are integers.
+		To declare a HashMap with this configuration,
+		you can use the following syntax:
+
+		*/
+
+
+
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+
+		// start of work day, initialize all bins to empty
+		// these are key value pairs
+		map.put("USA-Bin", 0);
+		map.put("Japan-Bin", 0);
+		map.put("Best-Korea-Bin", 0);
+		map.put("Vietnam-Bin",0);
+		map.put("HongKong-Bin", 0);
+
+		// Input data
+
+		String[][] binData = new String[7][2];
+
+		binData[0][0] = "USA-Bin";
+		binData[0][1] = "1";
+
+		binData[1][0] = "HongKong-Bin";
+		binData[1][1] = "2";
+
+		binData[2][0] = "USA-Bin";
+		binData[2][1] = "4";
+
+		binData[3][0] = "Best-Korea-Bin";
+		binData[3][1] = "6";
+
+		binData[4][0] = "Vietnam-Bin";
+		binData[4][1] = "2";
+
+		binData[5][0] = "Japan-Bin";
+		binData[5][1] = "2";
+
+		binData[6][0] = "Vietnam-Bin";
+		binData[6][1] = "2";
+
+
+		// Iterate through rows
+		for (int i = 0; i < binData.length; i++) {
+			// Iterate through columns in the current row
+			System.out.println("Country name = " + binData[i][0] + ", Number of packets = " + binData[i][1]);     // Move to the next line after each row
+			String binName = binData[i][0];
+			String numberOfPackets = binData[i][1];
+			int valueToAdd = Integer.parseInt(numberOfPackets);
+
+			// add number of packets to correct bin
+			int currentValue = map.get(binName);
+			int newValue = currentValue + valueToAdd;
+			map.put(binName, newValue);
+			System.out.println("New Value = " + newValue);
+
+
+		}
+
+
+		// Print all values
+		System.out.println(map);
+	}
+}
